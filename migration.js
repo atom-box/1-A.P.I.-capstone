d550e63a8951ff4156a1049347b26f86c4a23288
Evan Genest <genest.1@osu.edu>
2018-06-01 08:00:14 -0500

    Add error handling and DROPTABLE if exists to all three CREATE functions. Result is then,all three errors fire and then ALL THREE 'ALL IS WELL' messages fire\!  Could be worse...

--- migration.js
+++ migration.js
@@ -1,3 +1,6 @@
+/*to do next-action circa line 527 Drop table if exist language is in SQL-notes.  THEN if neccessary make the table droppable -- maybe that is only from some other POSTGRES or something...*/
+
+
 /*This file should hold all of the table set-up.
 Supplement it with the fake data in SEED.JS */
 const sqlite3 = require('sqlite3');
@@ -5,7 +8,16 @@ const db = new sqlite3.Database('./database.sqlite');
 
 function createArtist(){
 	console.log("MIGRATION TABLE PRESENT!");
-	db.run("DROP TABLE IF EXISTS Artist");
+	db.run("DROP TABLE IF EXISTS Artist",
+	function(e){
+		if(e){
+			console.log(`Problem, sir:${e}`);
+		}
+		else {
+			console.log(`All is well on line 17.`);
+		}
+	}
+	);
 	db.run(
 		"CREATE TABLE Artist (id INTEGER PRIMARY KEY NOT NULL,name TEXT NOT NULL ,date_of_birth TEXT NOT NULL, biography TEXT NOT NULL,is_currently_employed INTEGER DEFAULT 1)", 
 		error=>{if(error) {console.log('oops 1...')
@@ -16,16 +28,43 @@ function createArtist(){
 };
 
 function createSeries(){
-	db.run("CREATE TABLE Series (		id INTEGER PRIMARY KEY NOT NULL		,name TEXT NOT NULL, description TEXT NOT NULL)", (error)=>console.log('oops 2...')
+		db.run("DROP TABLE IF EXISTS Series",
+	function(e){
+		if(e){
+			console.log(`Problem, sir:${e}`);
+		}
+		else {
+			console.log(`All is well on line 37.`);
+		}
+	}
 	);
+
+	db.run("CREATE TABLE Series (		id INTEGER PRIMARY KEY NOT NULL		,name TEXT NOT NULL, description TEXT NOT NULL)", 
+		function(error){
+			if (error){
+				console.log(`oops 2...${error}`)
+			} else {
+				console.log('2 worked.  with old-style curlies')
+			}
+	});
 }
 
 function createIssue(){
+		db.run("DROP TABLE IF EXISTS Issue",
+	function(e){
+		if(e){
+			console.log(`Problem, sir:${e}`);
+		}
+		else {
+			console.log(`All is well on line 59.`);
+		}
+	}
+	);
+
 	db.run("CREATE TABLE Issue (		id INTEGER PRIMARY KEY NOT NULL		,name TEXT NOT NULL, issue_number TEXT NOT NULL, publication_date TEXT NOT NULL, artist_id INTEGER FOREIGN KEEEYYY NOT NULL, series_id INTEGER FOREIGN KEY NOT NULL)", 
 		(error)=>console.log('oops 3...')
 	);
 }
-
 
 
 module.exports = {createArtist, createSeries, createIssue}
