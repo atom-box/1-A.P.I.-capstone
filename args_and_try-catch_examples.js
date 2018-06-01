de1a7c5e6012c76c01e8471384b25b68e7854313
Evan Genest <genest.1@osu.edu>
2018-06-01 07:20:10 -0500

    Clearly write a comment at top explaining how the passed-in args work.

--- args_and_try-catch_examples.js
+++ args_and_try-catch_examples.js
@@ -4,30 +4,44 @@
 
 // DON'T EDIT.  THE FIVE EXAMPLES ARE USEFUL AT BOTTOM
 
-function fireworks(a){
+function fireworks(a){  
+// 6/1: key to understanding:
+// You're telling it, 'Grab the first thing passed in'.
 	let mudhens = [];
-	console.log("==========");
+
+	try{
+		console.log(`Passed in ${a}-thing has _${a.length}_ elements.`);
+	} catch(e) {
+		console.log(`errOne is ${e}`);
+		console.log(`errOne length is ${e.length}`);
+	}
+
 	try {
 		mudhens = Array.from(a);
-	} catch(error) {
-		console.log('sleep');
-		//return; 
-	}
-	try{
-		console.log(`Passed-in thing has _${a.length}_ elements.`);
-		console.log(`Arrayified created-thing has _${mudhens.length}_ elements.`);
 	} catch(e) {
-		// do nothing ha ha ha ha ha
+		console.log(`errTwo is ${e}`);
+		console.log(`errTwo length is ${e.length}`);
 	}
+
+	try{
+		console.log(`Arrayified created thing has _${mudhens.length}_ elements.`);
+	} catch(e) {
+		console.log(`errThree is ${e}`);
+		console.log(`errThree length is ${e.length}`);
+	}
+
+
 	for(let x of mudhens){
 		console.log(`[${x}] `);
 	}
+	console.log("==========");
 }
 
 
 // THE FOLLOWING ARE ALL SUCCESFUL
 fireworks("woof"); 		// works.  gives w-o-o-f
 fireworks(3, "woof"); 	// not work.   error is 100% silent!
+fireworks("woof", 3); 	// not work.   error is 100% silent!
 fireworks();			// not work.  runs the code in CATCH
 fireworks(undefined);	// not work.  runs the code in catch
 fireworks([3, "woof"]);	// works. gives 3 - woof
