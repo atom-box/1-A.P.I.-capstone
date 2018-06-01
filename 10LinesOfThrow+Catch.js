f8505b183a13dba69e4bcea4f2dc106f40f759a1
Evan Genest <genest.1@osu.edu>
2018-05-31 20:00:07 -0500

    Make the second error loop work.  First error loop fails silently.  (To Do: Compare it to the other sample error snippet in this folder...)

--- 10LinesOfThrow+Catch.js
+++ 10LinesOfThrow+Catch.js
@@ -1,2 +1,41 @@
 //Time out to throw and catch an error
 // @mistergenest May 31, 2018
+
+//receive args
+
+console.log("Begin.");
+console.log(arguments[0].length + "...was what I saw.");
+console.log("End.");
+
+const inputted = { virtue: "compassion",
+	pillars: 5};
+
+try{
+	let i = 0;
+	for (; i < inputted.virtue; i++){
+		console.log("First attempt to blow your mind.");
+	}
+} catch(e) 	{
+	if(e){
+	console.log("Poo -- there was an error.");
+
+	} else {
+	console.log("Yay -- there was no error.");
+
+	}
+}
+
+try{
+	i = 0;
+	for (; i < inputted.pillars; i++){
+		console.log("Second attempt to blow your mind.");
+	}
+} catch(e) 	{
+	if(e){
+	console.log("Poo -- there was an error.");
+
+	} else {
+	console.log("Yay -- there was no error.");
+
+	}
+}
