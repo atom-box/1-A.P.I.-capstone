5f5e461a616785e19c2349d6c7743f0f84a70b01
Evan Genest <genest.1@osu.edu>
2018-06-04 07:48:31 -0500

    Make a GET response for the root '/'.

--- server.js
+++ server.js
@@ -11,3 +11,13 @@ console.log(`server.js is listening.  On port number _${PORT}_`);
 createArtist();
 createSeries();
 createIssue();
+// concern: will these reinvent the db every single time a request comes in?  
+// not sure how these were hidden in Olympic.
+
+const path = require('path');
+const staticPath = path.resolve(__dirname, 'static');
+// so folders found on Windows systems
+
+app.get('/', (req, res)=>{
+	res.status(200).end("Coming soon, a proper back end to COMIC.");
+} );
