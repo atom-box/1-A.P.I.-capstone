d991249cee0c45d62736bd017033bbd2862d3b41
Evan Genest <genest.1@osu.edu>
2018-06-04 07:55:32 -0500

    Add logger.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // created by me, 100%
 // This is the main javascript that calls all else
 
-const express = require("express");
+const express = require('express');
+const logger = require('morgan');
 const app = express();
 const PORT = process.env.PORT || 4000;
 const {createArtist, createSeries, createIssue} = require('./migration.js');
 app.listen(PORT);
+app.use(logger('tiny'));
 console.log(`server.js is listening.  On port number _${PORT}_`);
 
 createArtist();
