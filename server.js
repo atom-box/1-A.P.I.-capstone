fdc1e7f6ee55bbcf117a019d63e4e8d34dbb28c4
Evan Genest <genest.1@osu.edu>
2018-06-14 06:11:20 -0500

    Add debugging print statement to get TYPE on route call api..

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 // created by me, 100%
 // This is the main javascript that calls all else
 
+/*
+
+
+THIS    CURRENTLY    CRASHES,   INSTEAD OF FINDING      API ROUTE
+THURSDAY MORNING I FOUND OUT THAT SECOND PART OF LINE 24 is currently an object but is supposed to be a function. 
+But did other people do like this?
+
+
+Next action may be to find Hahn syntax for this.  Is mine in wrong order? Missing a 'USE - require ?'
+*/
 const express = require('express');
-const logger = require('morgan');
 const app = express();
+
+const logger = require('morgan');
+//const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 4000;
-const setUp = require('./migration.js');
-const helper = require('./work/tools.js');
-app.listen(PORT);
+const routes = require('./work/routes.js');
+//app.use(bodyParser.json());
 app.use(logger('tiny'));
+//const todos = '/api';
+console.log(`Typeof ROUTES is ${routes} `);
+app.use('/api' , routes);
+// Crashing here!  Look in Hahn for syntax.
+app.listen(PORT);
+
+
 console.log(`server.js is listening.  On port number _${PORT}_`);
 
 
 
-const path = require('path');
-const staticPath = path.resolve(__dirname, 'static');
+//const path = require('path');
+//const staticPath = path.resolve(__dirname, 'static');
 
-app.get('/', (req, res)=>{
-	res.status(200).end("Coming soon, a proper back end to COMIC.");
-} );
+//app.get('/', (req, res)=>{
+//	res.status(200).end("Coming soon, a proper back end to COMIC.");
+//} );
 
 
 
