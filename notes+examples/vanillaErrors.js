612e3c61ff7eb95fb4dde4e307596f740d949f5b
Evan Genest <genest.1@osu.edu>
2018-06-18 18:18:15 -0500

    Print out to console.log the keys from the VANILLA REQUEST OBJECT.

--- notes+examples/vanillaErrors.js
+++ notes+examples/vanillaErrors.js
@@ -1,11 +1,15 @@
 const http = require('http');
+const PORT = 2112;
 const srvr = http.createServer((req, res)=>{
+	console.log(`I see    _${Object.keys(req)}_ &  _${PORT}_`);
 	//  stay tuned
 }  );
 
 srvr.on('req', (req, res) => {
+
 	//  coming soon, too
-const { method, url} = req;
+	const { method, url} = req;
+	console.log('I see    _${method}_ &  _${url}_');
 // Weird! Cool.  Surprising:
 // Note -- req is an OBJECT
 // Note -- req is an instance of IncomingMessage !! 
@@ -14,8 +18,12 @@ const { method, url} = req;
 
 } )
 
+srvr.listen(PORT);
+console.log(`Local  host   is    ${PORT}`);
 
-
+// sara is associate dean
+// dean is Japanese-American
+// that emily heller did the Action-Research on teacher learning cycle
 
 /*
 As we cheered #ElTri, 
