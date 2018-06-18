b3b39d8db5e5f6e6a0e87575923ec3bfd300802a
Evan Genest <genest.1@osu.edu>
2018-06-18 07:15:44 -0500

    Examine the req objece in detail by showing you can say CONST {METHOD, URL} = REQ;

---
+++ notes+examples/vanillaErrors.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const srvr = http.createServer((req, res)=>{
+	//  stay tuned
+}  );
+
+srvr.on('req', (req, res) => {
+	//  coming soon, too
+const { method, url} = req;
+// Weird! Cool.  Surprising:
+// Note -- req is an OBJECT
+// Note -- req is an instance of IncomingMessage !! 
+// Note -- this FAILED when outside this code-block.
+
+
+} )
+
+
+
+
+/*
+As we cheered #ElTri, 
+my subconcious mind imagined the Zimmerman telegram was accidentally delivered to Orotorio, who rebuffed it with a sprinting, daring, confident offense, rushing across the Atlantic, sending GER to the showers, ending WWII prematurely.
+
+A vigorous, daring response to the Zimmerman telegram, years later.
+*/
