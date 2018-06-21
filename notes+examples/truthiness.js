9647caac8c3e5968376d79e362efbf69288f63f9
Evan Genest <genest.1@osu.edu>
2018-06-21 06:29:00 -0500

    Make a sandbox for javascript truthiness syntax.

---
+++ notes+examples/truthiness.js
@@ -0,0 +1,47 @@
+var req.body.artist.id = 4;
+var
+;
+let x = 3;
+let req = null;
+req = {};
+req.a = 4;
+"a" in req
+"b" in req
+true == req.a
+req.a = 1
+true == req.a
+req.a = 2
+true == req.a
+let flag = true
+let flag2 = "true"
+let flag3 = 1
+let flag4 = ""
+true == true
+true == flag2
+true == flag3
+true == flag4
+let flag5 = 2
+true == flag5
+flag
+typeof flag
+typeof flag1
+typeof flag2
+typeof flag3
+flag == flag3
+flag === flag3
+null == undefined
+null == 0
+0 == false
+null == false
+2 == true
+2 == false
+let checkit = function(input){ if (input){console.log(`${input} is truthy`} else { console.log(`${input} is falsy`)}
+;
+let checkit = function(input){ if (input){console.log(`${input} is truthy`} else { console.log(`${input} is falsy`)};;
+
+
+
+
+let y = 4;
+function checkit (input){ if (input){console.log(`${input} is truthy`} else { console.log(`${input} is falsy`)};;
+
