13821385ca0d3655c7eef1026d2f61d6349de5d4
Evan Genest <genest.1@osu.edu>
2018-06-21 07:33:07 -0500

    Change incoming req.body properties to short var names and then do an error-check, via if NOT( && && && ) Refactor errors to all be called ERR for consistancy\!

--- work/routes.js
+++ work/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express();
 const sqlite = require(`sqlite3`);
-const db = new sqlite.Database(`./database.sqlite`);
+const db = new sqlite.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 router.get("/", (req, res, next)=>{
 	res.status(200).end("Close; insufficient info.\t\t\t\tProper useage: /api/artists  or /api/series ");
@@ -9,14 +9,47 @@ router.get("/", (req, res, next)=>{
 
 router.get(`/artists`, (req, res, next)=>{
 	//let rows = null;
-	db.all(`SELECT * FROM Artist WHERE Artist.is_currently_employed = 1;`, (error, artists)=> {
-		if (error){
-			console.log(`In artists route -->${error}`);
-			return null;
+	db.all(`SELECT * FROM Artist WHERE Artist.is_currently_employed = 1;`, (err, artists)=> {
+		if (err){
+			next(`In artists route -->${err}`);
 		}	
     res.status(200).json( {"artists": artists} );
 	});
 });
+
+router.post('/artists',(req, res, next) => {
+
+  const i = req.body.artist.id, 
+    name = req.body.artist.name, 
+    dob = req.body.artist.dateOfBirth, 
+    bio = req.body.artist.biography , 
+    employed = req.body.artist.is_currently_employed;
+
+
+
+
+
+
+  if (!( i && name && dob && bio )){
+    next(new Error `Some missing input in POST ARTISTS`); // hope this trickles out of the bottom of ROUTES.JS and finds the error-handling bottom of SERVER.JS
+  }
+  const query = `INSERT INTO Artist (id, name, date_of_birth, biography, is_currently_employed) VALUES ($i, $n, $dob, $b, $e);`; 
+  db.put(query, {
+    $i: i, 
+    $n: name, 
+    $dob: dob, 
+    $b: bio , 
+    $e: employed  }, 
+    (err)=>{
+      console.log(`Ham.  Swiss.  ${e}. `);
+    if (err) {
+      next(err);
+      //console.error(e);
+    }
+  } );
+} );
+
+//my especially useless error handler is in main server.js.
 
 
 /*helper.getArtists = () => {
