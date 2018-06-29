8a4fd654f395d8b69d92404a12ce56ee10ffd036
Evan Genest <genest.1@osu.edu>
2018-06-29 07:31:31 -0500

    Make a function, LOOGOO that returns as a string several req objects, incl URL, DOMAIN, PARAMS.

--- notes+examples/logger-homemade.js
+++ notes+examples/logger-homemade.js
@@ -6,12 +6,26 @@
 const express = require("express");
 const app = express();
 
-function loogoo(){
-	
+function loogoo(req){
+	let message = [];
+	// make MESS an object -> json later
+	message.push(['hey', 'nanny', 'hey', 'now']);
+	message.push([
+		'REQ.URL:',
+		req.url,
+		'REQ.METHOD:',
+		req.method,
+		'REQ.PARAMS:',
+		req.params,
+		'EVERYTHING ON REQ.PARAMS:',
+		Object.keys(req.params)
+	]);
+	const out = message.toString();
+	return out;
 }
 
 app.get(`/hilldale`, (req, res)=>{
-	res.send(`The bananas are full price.`);
+	res.send(`The bananas are full price. Here is the output of the LOOGOO logger function you made: ${loogoo(req)}`);
 });
 app.get(`/`, (req, res, next)=>{
 	res.status(202).write("T.E.E.");
