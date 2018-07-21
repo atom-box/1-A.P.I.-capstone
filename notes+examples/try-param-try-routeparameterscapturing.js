7c301f1211cf3a9a2be1ea0d4ddfd7c64c4f3656
Evan Genest <genest.1@osu.edu>
2018-07-21 18:38:16 -0500

    Make the routes '/' and 'artists/id/names/id' work.  Still not working: the param function.

--- notes+examples/try-param-try-routeparameterscapturing.js
+++ notes+examples/try-param-try-routeparameterscapturing.js
@@ -6,16 +6,37 @@
 
 const express = require('express');
 const api = express();
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 
 // https://expressjs.com/en/api.html#app.param 
 // for syntax of PARAM!
 //     DO    API.PARAM
 
-api.get('/users/:userId/books/:bookId', (req, res) => {
+var route1 = '/users/:userId/books/:bookId';
+var route2 = '/cherry'
+var flag = 9;
+function incrFlag(cat){
+	flag += cat;
+	console.log(`Flag is: ${flag} `);
+}
+
+api.param(route2 , (req, res, next, val, name)=> {
+	console.log(`Hi there mister${x}thing.`);
+	console.log(`X is: ${x}` );	
+	incrFlag("param-for-route2");
+	next();
+} );
+
+api.get(route1, (req, res) => {
+	incrFlag("route1")
 	res.send(req.params);
 } );
 
+api.get('/', (req, res)=> {
+	incrFlag("empty route");
+	res.send(`Proper useage is: ${route1} `);
+} );
+
 const PORT = 3939;
-api.listen(PORT, console.log(`Doin it to it in ${PORT}`) );
+api.listen(PORT, console.log(`Doin it to it in ${PORT} Usage is _${route1}_`) );
 
