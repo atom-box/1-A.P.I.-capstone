a78973808097b5956af8ad7b7807af331073eb2d
Evan Genest <genest.1@osu.edu>
2018-07-23 08:14:02 -0500

    Make a rudimentary 'Hello World' type of Express object.

--- notes+examples/julyExprAndNodeSnippets.js
+++ notes+examples/julyExprAndNodeSnippets.js
@@ -21,9 +21,23 @@ let a;
 console.log(a); // UNDEFINED, b/c a is global
 console.log(b); // 7, b/c b is global
 
-/*
-		
+/* https://www.sitepoint.com/5-typical-javascript-interview-exercises/
+Gotcha for scope and declaring variables in JS.
 */
 
-/* https://www.sitepoint.com/5-typical-javascript-interview-exercises/
+const express = require(`express`);
+const app = express();
+const route0 = `/`;
+const noun0 = `sauce`;
+app.get(route0, (req, res)=>{
+	res.end(`${noun0}, no ${noun0}...`);
+} );
+const PORT = 2055;
+app.listen(PORT, (req, res)=>{
+	console.log(`Server is looking for ${noun0} on localhost: ${PORT}`);
+} );
+
+/*
+	https://medium.com/@adamzerner/how-bodyparser-works-247897a93b90
+	Dude explains middleware and then BODYPARSER for the req object
 */
